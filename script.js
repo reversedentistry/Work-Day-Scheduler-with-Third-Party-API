d57e4821ad57552156cd2fb424f40c6629f67172
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-05-06 13:50:58 -0700

    Started new function to iterate each timeblock row

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 var currentDay = document.querySelector("#currentDay"); 
+var currentTime = moment().format()
+var startHour = moment("9AM, h:mm");
 
-currentDay.innerText = moment().format("dddd, MMMM Do, YYYY"); 
+const scheduleContainer = $(".container");
+const workDayHours = 9; 
+
+currentDay.textContent = moment().format("dddd, MMMM Do, YYYY"); 
+
+function currentSchedule () {
+    for (let i = 0; i < workDayHours; i++) {
+    
+    let timeBlock = $("<div>");
+    timeBlock.addClass("time-block", "row"); 
+
+    let hour = $("<label>"); 
+    hour.text(startHour.format("h a")); 
+    hour.attr("for", "block-desc"); 
+    hour.addClass("hour"); 
+
+    let textArea = $("<textarea>"); 
+    textArea.attr("id", "block-desc"); 
+    textArea.attr("name", "block-desc"); 
+
+    let saveButton = $("<button>"); 
+    saveButton.addClass("saveBtn");
+ // can I add a font-awesome attribute/class the same way???    
+    let saveBtnIcon = $("<i>"); 
+    saveBtnIcon.addClass("fa-solid fa-floppy-disk"); 
+
+
+}
+}
+
+
+// <i class="fa-solid fa-floppy-disk"></i>
+
